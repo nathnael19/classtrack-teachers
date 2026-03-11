3542322be28376d81a321012ae80a9dc07721b33
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-03-11 03:01:37 +0300

    Add initial App component, styles, and global CSS configuration

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'sonner';
+import { router } from './routes';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <Toaster position="top-right" richColors />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
